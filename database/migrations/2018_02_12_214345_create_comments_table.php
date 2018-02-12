c3c76f0b92f15c987cf50d4f74b81ab07b0ff6dd
田村光隆 <terunaga_tamura@hotmail.co.jp>
2018-02-12 21:48:42 +0900

    create comment model and corresponding migration file

---
+++ database/migrations/2018_02_12_214345_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    protected $table = 'comments';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('entry_id', false, true);
+            $table->string('name', 85)->nullable();
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
